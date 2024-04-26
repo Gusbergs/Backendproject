98be592358691b6bc661ad6ac1a812b5830327b0
KritsadakornMoonmai <kritsadakorn.moonmai@outlook.com>
2024-04-26 13:41:20 +0200

    add navbar with Service button(dropbox)

--- src/main/java/com/example/backendproject/models/Room.java
+++ src/main/java/com/example/backendproject/models/Room.java
@@ -4,11 +4,16 @@ package com.example.backendproject.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class Room {
 
@@ -24,6 +29,9 @@ public class Room {
 
     int extraBed;
 
+
+    @OneToMany(mappedBy = "room")
+    private List<Booking> bookingList;
 
     public Room(int roomNumber, boolean doubleRoom, int extraBed){
         this.roomNumber = roomNumber;

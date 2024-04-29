0da3f9fa2fa0064b292e086476d35aba45927dfb
Filip Stetter <filip.stetter@telia.se>
2024-04-29 14:14:22 +0200

    Save

--- src/main/java/com/example/backendproject/controller/RoomController.java
+++ src/main/java/com/example/backendproject/controller/RoomController.java
@@ -1,21 +1,30 @@
 package com.example.backendproject.controller;
 
-
-import com.example.backendproject.service.BookingService;
+import com.example.backendproject.dto.RoomDtoMini;
 import com.example.backendproject.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/Bookings")
+@RequestMapping("/rooms")
 public class RoomController {
 
+    private final RoomService roomService;
 
     @Autowired
-    private RoomService roomService;
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
 
-
-
-
+    @GetMapping("/all")
+    public String getAllRooms(Model model) {
+        List<RoomDtoMini> rooms = roomService.getAllRoomsMini();
+        model.addAttribute("rooms", rooms);
+        return "rooms";
+    }
 }

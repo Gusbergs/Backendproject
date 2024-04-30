a5e872013a1c0bc02d7ee0a1cba68c9ae9c3fa4f
KritsadakornMoonmai <kritsadakorn.moonmai@outlook.com>
2024-04-30 13:16:54 +0200

    add timeconfilcted function that prevent booking om booked time room

---
+++ src/test/java/com/example/backendproject/controller/BookingControllerTest.java
@@ -0,0 +1,78 @@
+package com.example.backendproject.controller;
+
+import com.example.backendproject.dto.BookingDtoDetailed;
+import com.example.backendproject.dto.RoomDtoDetailed;
+import com.example.backendproject.models.Room;
+import com.example.backendproject.repo.BookingRepo;
+import com.example.backendproject.repo.RoomRepo;
+import com.example.backendproject.service.BookingService;
+import com.example.backendproject.service.RoomService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class BookingControllerTest {
+
+    @Autowired
+    BookingController bookingController;
+    BookingService bookingService;
+
+    @Autowired
+    RoomService roomService;
+
+    @Autowired
+    RoomRepo roomRepo;
+
+
+
+    @Test
+    void findCrossedTime() {
+        LocalDate startDate = LocalDate.of(2020, 10, 1);
+        LocalDate stopDate = LocalDate.of(2020, 10, 5);
+
+        LocalDate startDate2 = LocalDate.of(2022, 1, 2);
+        LocalDate stopDate2 = LocalDate.of(2023, 3, 21);
+
+        LocalDate startDate3 = LocalDate.of(2022, 12, 2);
+        LocalDate stopDate3 = LocalDate.of(2023, 2, 16);
+
+        RoomDtoDetailed roomDtoDetailed = null;
+
+        boolean isTheRoomExist = false;
+        for (RoomDtoDetailed getRoom : roomService.getAllRoomsDetailed()) {
+            if (getRoom.getId() == 154L) {
+                roomDtoDetailed = getRoom;
+                isTheRoomExist = true;
+                break;
+            }
+        }
+        if (isTheRoomExist) {
+            System.out.println(roomDtoDetailed.getId() + " " + roomDtoDetailed.getRoomNumber());
+        } else {
+            System.out.println("Can't find the id '154'");
+        }
+
+        System.out.println(bookingController.findCrossedTime(startDate, stopDate, roomDtoDetailed));
+        System.out.println(bookingController.findCrossedTime(startDate2, stopDate2, roomDtoDetailed));
+
+        assertTrue(bookingController.findCrossedTime(startDate, stopDate, roomDtoDetailed));
+        assertFalse(bookingController.findCrossedTime(startDate2, stopDate2, roomDtoDetailed));
+        assertTrue(bookingController.findCrossedTime(startDate3, stopDate3, roomDtoDetailed));
+    }
+
+    @Test
+    public void showAllBookingDtoDetailed() {
+        for (RoomDtoDetailed getRoom : roomService.getAllRoomsDetailed()) {
+            System.out.println(getRoom.getId() + " " + getRoom.getRoomNumber() + " " + getRoom.getExtraBed());
+        }
+    }
+}

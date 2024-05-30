109ecad538c7aba4f0e20a9db479b5e5d520c558
KritsadakornMoonmai <kritsadakorn.moonmai@outlook.com>
2024-05-30 14:11:16 +0200

    Reset password

--- src/main/java/com/example/backendproject/controller/PasswordResetController.java
+++ src/main/java/com/example/backendproject/controller/PasswordResetController.java
@@ -4,8 +4,10 @@ import com.example.backendproject.models.User;
 import com.example.backendproject.repo.UserRepo;
 import com.example.backendproject.service.PasswordResetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,9 +31,16 @@ public class PasswordResetController {
     }
 
     @PostMapping("/forgot-password")
-    public String handleForgotPassword(@RequestParam("email") String email) {
-        passwordResetService.sendPasswordResetEmail(email);
-        return "redirect:/forgot-password?success";
+    public String handleForgotPassword(@RequestParam("email") String email, Model model) {
+        User user = userRepository.findByUsername(email);
+        if (user != null) {
+            passwordResetService.sendPasswordResetEmail(email);
+            return "redirect:/forgot-password?success";
+        } else {
+            model.addAttribute("error_message", "E-posten finns inte");
+            model.addAttribute("isAvailable", false);
+            return "Forgot-Password.html";
+        }
     }
 
     @GetMapping("/reset-password")
